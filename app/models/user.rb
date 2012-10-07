70085ec4ff9f50f480bc6156f83fe14cb7a29683
Landria <landria@linux-4tpj.site>
2012-10-07 03:45:17 +0000

    -devise password confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :remember_me
   attr_protected :is_locked
 
   ROLES = %w[admin guest user]
