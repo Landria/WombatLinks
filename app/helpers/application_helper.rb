570e3bacf514024ed9f2a3d9c0d6bf9512362ca1
Natalia Sergeeva <natalia.m.sergeeva@gmail.com>
2012-12-15 01:30:07 +0400

    Page meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def title(page_title)
+  def title page_title
     content_for(:title) { page_title }
+  end
+
+  def description
+    t 'site.description'
+  end
+
+  def keywords
+    t 'site.keywords'
   end
 end
