19a33a8794800c7fccd71cec81dd5aa8bd0c5356
Natalia Sergeeva <natalia.m.sergeeva@gmail.com>
2013-01-15 16:56:35 +0400

    mail job delay

--- app/jobs/mail_link_job.rb
+++ app/jobs/mail_link_job.rb
@@ -4,6 +4,7 @@ class MailLinkJob < Resque::Job
 
   def self.perform user_link_id
     link = UserLink.find(user_link_id)
+    sleep 30
     WombatMailer.send_link(link).deliver
     link.update_attribute(:is_send, true)
   rescue
